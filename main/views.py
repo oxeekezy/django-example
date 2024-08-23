25018278160e9d0167c7fb2e674752089e031da7
oxeek <workkukin@gmail.com>
2024-08-23 15:01:41 +0300

    Добавлены базовые функции отображения

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request) -> HttpResponse:
+    return HttpResponse("Home Page")
+
+
+def about(request) -> HttpResponse:
+    return HttpResponse("About this page")
