a9f6b5dc938008728f2aff618689d34135ca5e60
oxeek <workkukin@gmail.com>
2024-09-03 17:31:48 +0300

    Добавлено отображение категорий в base.html. Сделаны дампы

--- main/views.py
+++ main/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from merch.models import Categories
 
 
 def index(request) -> HttpResponse:
+
+    categories = Categories.objects.all()
+
     context = {
         "title": "Welcome to Cheglock",
         "name": "Cheglock Sokol Creation",
         "motto": "Сообщество креативных людей",
+        "categories": categories,
     }
 
     return render(request, "main/index.html", context)
