ac6719cdcd5bfa26014dfacced226faa65a1ac4f
oxeek <workkukin@gmail.com>
2024-09-04 14:45:09 +0300

    Перенесена логика регистрации моделей в соответсвующее приложение

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from merch.models import Categories, Products
 
 
-@admin.register(Categories)
-class AdminCategories(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
 
-@admin.register(Products)
-class AdminProducts(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
 
