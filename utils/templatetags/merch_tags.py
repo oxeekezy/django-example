14683a505586f953b5e21f93cdb39aeb197e1659
oxeek <workkukin@gmail.com>
2024-09-04 14:39:23 +0300

    Добавлено приложение utils, перенесены пользовательские теги

--- utils/templatetags/merch_tags.py
+++ utils/templatetags/merch_tags.py
@@ -4,6 +4,7 @@ from django import template
 register = template.Library()
 
 
+# Шаблонный тег для получения категорий
 @register.simple_tag()
 def categories_tag():
     return Categories.objects.all()
