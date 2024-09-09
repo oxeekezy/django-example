a469d212a6d829442845e3573b8b82f553a19789
oxeek <workkukin@gmail.com>
2024-09-09 14:35:16 +0300

    Исправлена логика получения всех товаров. Убран путь специальный путь

--- merch/urls.py
+++ merch/urls.py
@@ -4,7 +4,6 @@ from merch import views
 app_name = "merch"
 
 urlpatterns = [
-    path("", views.get_all_catalog, name="all_catalog"),
     path("<slug:slug>/", views.catalog, name="catalog"),
     path("product/<slug:slug>/", views.product, name="product"),
 ]
--- merch/views.py
+++ merch/views.py
@@ -8,21 +8,16 @@ def catalog(request, slug):
     discount = request.GET.get("discount", False)
     order_by = request.GET.get("order_by", None)
 
-    merch = Products.objects.filter(category__slug=slug)
+    if slug == "all":
+        merch = Products.objects.all()
+    else:
+        merch = Products.objects.filter(category__slug=slug)
 
     if discount:
         merch = merch.filter(discount__gt=0)
 
     if order_by:
         merch = merch.order_by(order_by)
-
-    context = BASE_CONTEXT | {"merch": merch}
-
-    return render(request, "merch/catalog.html", context)
-
-
-def get_all_catalog(request):
-    merch = Products.objects.all()
 
     context = BASE_CONTEXT | {"merch": merch}
 
